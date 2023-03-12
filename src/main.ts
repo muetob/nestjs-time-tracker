9ac97ba458d0477a91edfaafaabd6782b0ee76ef
Tobias Mueller <tobias.mueller@hostpoint.ch>
2023-03-12 22:05:47 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(7777);
 }
 
