01e49fe7f9cd44c06f28664dd065ef04dae824e5
Tobias Mueller <tobias.mueller@hostpoint.ch>
2023-03-12 21:23:42 +0100

    Remove ormconfig

--- src/write-type-orm-config.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-import dbConfig from './config/db.config';
-
-try {
-  fs.unlinkSync('ormconfig.json');
-} catch {}
-
-fs.writeFileSync('ormconfig.json', JSON.stringify(dbConfig(), null, 4));
